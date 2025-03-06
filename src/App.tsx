3261ca404501c4c2bb1211c310e02642b4b4179f
Josh Leslie <hello@joshmleslie.com>
2025-03-06 18:49:17 -0500

    stub app html add is-mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { isMobile as testMobile } from 'is-mobile';
 import './App.css';
 
 function App() {
-	return <div id="app-container"></div>;
+	const mobileTest = testMobile();
+
+	return (
+		<div id="app-container">
+			<div
+				className={mobileTest ? 'mobile' : 'desktop'}
+				id="chart-select-bar"
+			></div>
+			<div id="chart-super-container">
+				<div id="selected-chart"></div>
+				<div id="selected-chart-blurb"></div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
