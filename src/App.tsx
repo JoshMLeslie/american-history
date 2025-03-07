04e8558cd605179f0f4457ac04d669e9cbec105c
Josh Leslie <hello@joshmleslie.com>
2025-03-07 12:21:21 -0500

    move mobile test to body / before app render add basic height/width css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { isMobile as testMobile } from 'is-mobile';
 import './App.css';
 
 function App() {
-	const mobileTest = testMobile();
-
 	return (
-		<div id="app-container">
-			<div
-				className={mobileTest ? 'mobile' : 'desktop'}
-				id="chart-select-bar"
-			></div>
+		<>
+			<div id="chart-select-bar"></div>
 			<div id="chart-super-container">
 				<div id="selected-chart"></div>
 				<div id="selected-chart-blurb"></div>
 			</div>
-		</div>
+		</>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+import { isMobile } from 'is-mobile';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+if (isMobile()) {
+	document.body.classList.add('mobile');
+}
 
 createRoot(document.getElementById('root')!).render(<App />);
