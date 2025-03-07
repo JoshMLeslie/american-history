b800567d779a4210f3f698dfdfe3261144e2f42a
Josh Leslie <hello@joshmleslie.com>
2025-03-07 12:37:29 -0500

    stub out basic css layout add chart-blurb expand toggle move app.css to scss, add relative npm pkg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+	const [showMoreBlurb, setShowMoreBlurb] = useState(false);
+	const toggleBlurb = () => setShowMoreBlurb(!showMoreBlurb);
+
 	return (
 		<>
 			<div id="chart-select-bar"></div>
 			<div id="chart-super-container">
 				<div id="selected-chart"></div>
-				<div id="selected-chart-blurb"></div>
+				<div
+					id="selected-chart-blurb"
+					className={showMoreBlurb ? 'expand' : ''}
+				>
+					<button id="chart-blurb-more" onClick={toggleBlurb}>
+						{showMoreBlurb ? 'Less' : 'More'}
+					</button>
+					<div id="chart-blurb-content"></div>
+				</div>
 			</div>
 		</>
 	);
