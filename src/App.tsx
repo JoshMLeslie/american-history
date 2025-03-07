1bd8c7ea145587523ab52e4208f8e9bff5bc2a65
Josh Leslie <hello@joshmleslie.com>
2025-03-07 13:49:40 -0500

    stub chart selection buttons and handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
 import { useState } from 'react';
 import './App.scss';
+import cc from './data/chart-map.json';
+
+type ChartCache = Record<SelectedChart['id'], SelectedChart>;
+const chartCache = cc as ChartCache;
+
+interface SelectedChart {
+	title: string;
+	description: string;
+	id: string;
+	data: any[];
+}
 
 function App() {
+	const [selectedChart, setSelectedChart] = useState<null | SelectedChart>();
 	const [showMoreBlurb, setShowMoreBlurb] = useState(false);
 	const toggleBlurb = () => setShowMoreBlurb(!showMoreBlurb);
 
+	const selectChart = (chartID: SelectedChart['id']) => () => {
+		setSelectedChart(chartCache[chartID]);
+	};
+
 	return (
 		<>
-			<div id="chart-select-bar"></div>
+			<div id="chart-select-nav">
+				<button onClick={selectChart('one')}>Chart One</button>
+				<button onClick={selectChart('two')}>Chart Two</button>
+				<button onClick={selectChart('three')}>Chart Three</button>
+				<button onClick={selectChart('four')}>Chart Four</button>
+			</div>
 			<div id="chart-super-container">
 				<div id="selected-chart"></div>
 				<div
 					id="selected-chart-blurb"
 					className={showMoreBlurb ? 'expand' : ''}
 				>
-					<button id="chart-blurb-more" onClick={toggleBlurb}>
-						{showMoreBlurb ? 'Less' : 'More'}
-					</button>
-					<div id="chart-blurb-content"></div>
+					<div id="chart-blurb-actions">
+						<h2>{selectedChart?.title}</h2>
+						<button id="chart-blurb-more" onClick={toggleBlurb}>
+							{showMoreBlurb ? 'Less' : 'More'}
+						</button>
+					</div>
+					<div id="chart-blurb-content">{selectedChart?.description}</div>
 				</div>
 			</div>
 		</>
