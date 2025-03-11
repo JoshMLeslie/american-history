880b0adb6803ec9a858aa4c32582edf209963c66
Josh Leslie <hello@joshmleslie.com>
2025-03-11 17:24:02 -0400

    compact color logic ternary formatting

--- src/charts/NetPersonalWealth.chart.tsx
+++ src/charts/NetPersonalWealth.chart.tsx
@@ -8,28 +8,33 @@ import {
 	XAxis,
 	YAxis,
 } from 'recharts';
-import { CHART_DEFAULT, chartReducer, genLineHSLColor, loadChartData } from './chart.util';
+import {
+	CHART_DEFAULT,
+	chartReducer,
+	genLineHSLColor,
+	loadChartData,
+} from './chart.util';
 
 const COUNTRY_PERCENTILES = [
-	"Eastern Europe-Top 1%",
-	"Eastern Europe-Bottom 50%",
-	"Western Europe-Top 1%",
-	"Western Europe-Bottom 50%",
-	"North America-Top 1%",
-	"North America-Bottom 50%",
-	"North Africa-Top 1%",
-	"North Africa-Bottom 50%",
-	"South Africa-Top 1%",
-	"South Africa-Bottom 50%",
-	"East Asia-Top 1%",
-	"East Asia-Bottom 50%",
-	"Russia & Central Asia-Top 1%",
-	"Russia & Central Asia-Bottom 50%",
-	"South & South-East Asia-Top 1%",
-	"South & South-East Asia-Bottom 50%",
-	"West Asia-Top 1%",
-	"West Asia-Bottom 50%",
-]
+	'Eastern Europe-Top 1%',
+	'Eastern Europe-Bottom 50%',
+	'Western Europe-Top 1%',
+	'Western Europe-Bottom 50%',
+	'North America-Top 1%',
+	'North America-Bottom 50%',
+	'North Africa-Top 1%',
+	'North Africa-Bottom 50%',
+	'South Africa-Top 1%',
+	'South Africa-Bottom 50%',
+	'East Asia-Top 1%',
+	'East Asia-Bottom 50%',
+	'Russia & Central Asia-Top 1%',
+	'Russia & Central Asia-Bottom 50%',
+	'South & South-East Asia-Top 1%',
+	'South & South-East Asia-Bottom 50%',
+	'West Asia-Top 1%',
+	'West Asia-Bottom 50%',
+];
 
 const NetPersonalWealthChart: React.FC = () => {
 	const [selectedState, dispatch] = useReducer(chartReducer, {
@@ -59,13 +64,13 @@ const NetPersonalWealthChart: React.FC = () => {
 				/>
 				<YAxis
 					allowDataOverflow
-					domain={([dataMin, dataMax]) => ([Math.round(dataMin) - 2, Math.round(dataMax) + 2])}
+					domain={['auto', 'auto']}
 					type="number"
 					yAxisId="1"
 				/>
 				{COUNTRY_PERCENTILES.map((country, i) => {
 					// since country percentile data is adjacent, normalize the color per country
-					const useColor = i % 2 === 0 ? genLineHSLColor(i) : genLineHSLColor(i-1);
+					const useColor = genLineHSLColor(i % 2 === 0 ? i : i - 1);
 					return (
 						<Line
 							key={country}
