77596b9dad257adaaf2be5d3ef6ba7e222971d20
Josh Leslie <hello@joshmleslie.com>
2025-03-15 17:17:54 -0400

    update TMR description

--- src/data/chart-meta.ts
+++ src/data/chart-meta.ts
@@ -17,8 +17,7 @@ export const chartMeta: ChartMeta = {
 	tmr: {
 		data: null,
 		title: 'Top Marginal Rate',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+		description: 'A historical look at top marginal income tax rate',
 		id: 'tmr',
 	},
 	npw: {
