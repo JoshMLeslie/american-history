93ee5b43c6973deefe4a18c63dc92d9713bdc559
Josh Leslie <hello@joshmleslie.com>
2025-03-15 13:30:29 -0400

    add 'esc' handling to close blurb add home path to nav list

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Link, Outlet, useLocation, useNavigate } from 'react-router';
 import './App.scss';
 import { ChartKeys, chartMeta } from './data/chart-meta';
@@ -7,7 +7,25 @@ function AppLayout() {
 	const navigate = useNavigate();
 	const location = useLocation();
 
+	const [chartId, setChartId] = useState<string>('home');
+	const [showMoreBlurb, setShowMoreBlurb] = useState(false);
+	const toggleBlurb = () => setShowMoreBlurb((s) => !s);
+
+	const handleEscBlurb = useCallback((e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			setShowMoreBlurb(false);
+		}
+	}, []);
+
 	useEffect(() => {
+		document.addEventListener('keydown', handleEscBlurb);
+		return () => {
+			document.removeEventListener('keydown', handleEscBlurb);
+		};
+	}, []);
+
+	useEffect(() => {
+		// handle redirect for limited rendering e.g. github pages
 		const redirectPath = sessionStorage.getItem('redirectFromExternal');
 		if (redirectPath) {
 			sessionStorage.removeItem('redirectFromExternal');
@@ -25,13 +43,16 @@ function AppLayout() {
 		}
 	}, [location.pathname]);
 
-	const [chartId, setChartId] = useState<string>('');
-	const [showMoreBlurb, setShowMoreBlurb] = useState(false);
-	const toggleBlurb = () => setShowMoreBlurb(!showMoreBlurb);
-
 	return (
 		<>
 			<div id="chart-select-nav">
+				<Link
+					className="button"
+					to='/'
+					onClick={() => setChartId(ChartKeys.HOME)}
+				>
+					Home
+				</Link>
 				<Link
 					className="button"
 					to={`/chart/${ChartKeys.TMR}`}
@@ -57,11 +78,9 @@ function AppLayout() {
 				>
 					<div id="chart-blurb-actions">
 						<h2>{chartMeta[chartId]?.title}</h2>
-						{chartMeta[chartId]?.description.length > 500 && (
-							<button id="chart-blurb-more" onClick={toggleBlurb}>
-								{showMoreBlurb ? 'Less' : 'More'}
-							</button>
-						)}
+						<button id="chart-blurb-more" onClick={toggleBlurb}>
+							{showMoreBlurb ? 'Shrink' : 'Expand'}
+						</button>
 					</div>
 					<p id="chart-blurb-content">{chartMeta[chartId]?.description}</p>
 				</div>
