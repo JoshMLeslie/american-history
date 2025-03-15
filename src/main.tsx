19b78096c498c26ca7e8320d75bbd7a4d5545507
Josh Leslie <hello@joshmleslie.com>
2025-03-15 13:50:58 -0400

    add basic css class based on mobile orientation

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const handleOrientation = () => {
+	document.body.classList.remove('portrait', 'landscape');
+	const orient = screen.orientation.type.split('-')[0]; // ignore 'upside down'
+	document.body.classList.add(orient);
+};
+
 if (isMobile()) {
 	document.body.classList.add('mobile');
+	handleOrientation();
+	screen.orientation.onchange = handleOrientation;
 }
 
 createRoot(document.getElementById('root')!).render(<App />);
